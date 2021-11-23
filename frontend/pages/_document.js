d67a6c078d7c2af4691ef5e38ad986b5598621eb
kapit0shka <neverlle98@gmail.com>
2021-11-23 23:10:23 +0500

    Update dependancises

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,10 +1,15 @@
 import Document, { Html, Head, Main, NextScript } from "next/document"
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
   render() {
     return (
       <Html lang="ru">
-        <Head></Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
